9a04e68a02c9d4c58b951e4bc701397c84989d5b
Wen Bao Yin <supermq@gmail.com>
2025-03-05 17:49:05 +0800

    chore: add webservice

---
+++ examples/text_semantic_search_server.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from similarities import BertSimilarity
+
+app = FastAPI()
+
+class RequestData(BaseModel):
+    sentences: list
+    corpus: list
+
+@app.post("/semantic_search")
+async def semantic_search(data: RequestData):
+    model = BertSimilarity(model_name_or_path="shibing624/text2vec-base-chinese")
+    model.add_corpus(data.corpus)
+    res = model.most_similar(queries=data.sentences, topn=3)
+    return {data.sentences[i]: c for i, c in enumerate(res)}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=6000)
